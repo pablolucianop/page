e1e7a09f959970cff0273faab090bb82f703dfc2
pablolucianop <pablolucianop@gmail.com>
2022-09-07 17:30:08 -0300

    no api, git ignotre

--- api/routes/v0/testAPI.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', function (req, res, next) {
-  res.send('API is working properly2 3')
-})
-
-module.exports = router
