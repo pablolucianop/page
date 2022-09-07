17d46daa460c337911f433846589365b8a36fe1f
pablolucianop <pablolucianop@gmail.com>
2022-09-07 17:33:08 -0300

    api with node modules built inn

---
+++ api/devMockup/users.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    id: 'it-drixit-1',
+    avatar:
+      'https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png',
+    email: 'it@drixit.com',
+    password: '$2b$10$tUG8FfBnrs8weiD53JFv3OlbP2Ty60tAkWVNutGYPQ7Rvu8d/Zx7i',
+    name: 'IT',
+    surname: 'Drixit',
+    age: 25,
+    role: 'admin',
+  },
+  {
+    id: 'info-drixit-2',
+    avatar:
+      'https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png',
+    email: 'info@drixit.com',
+    password: '$2b$10$s0V/hCfQU89qwXKKFrQgbeCiYSINaUBDETT3Nc0/CmblhCO9VupO.',
+    name: 'Info',
+    surname: 'Drixit',
+    age: 30,
+    role: 'user',
+  },
+]
+module.exports = users
